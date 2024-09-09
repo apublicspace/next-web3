419018235c38dc2956ad46e4c6032fb13b4a65ae
Sam Larsen <sam@slarsen.io>
2024-09-09 00:57:25 -0400

    Not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+	return (
+		<div className="flex items-center justify-center min-h-screen w-full">
+			<h1 className="text-2xl">404</h1>
+			<div className="h-12 w-[1px] bg-neutral mx-5" />
+			<h1 className="text-sm">This page could not be found.</h1>
+		</div>
+	);
+}
