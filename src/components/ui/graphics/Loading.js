d474a3d6c5bbe3bec6a499358a7e831da4384fde
Sam Larsen <sam@slarsen.io>
2024-09-09 01:07:33 -0400

    User interface graphics

---
+++ src/components/ui/graphics/Loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="fixed inset-0 flex items-center justify-center">
+			<span className="loading loading-spinner loading-lg" />
+		</div>
+	);
+}
